701b16667b33d7bf6c39857b2dc38c725cb25143
Ragnar B. Johannsson <ragnar@igo.is>
2014-02-25 20:42:36 +0000

    Debian install script

---
+++ deb-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Packages 
+sudo apt-get update
+cat << EOF | xargs sudo apt-get install -y 
+arping
+build-essential
+curl
+duplicity
+duply
+ec2-api-tools
+git
+golang
+htop
+httping
+iftop
+ioping
+mercurial
+moreutils
+mtr
+nmap
+nodejs
+pssh
+s3cmd
+subversion
+vim
+zsh
+EOF
+
+# Clone the dotfiles repo and install
+git clone https://github.com/ragnar-johannsson/dotfiles.git $HOME/.dotfiles
+cd $HOME/.dotfiles && git submodule init && git submodule update && ./setup.sh
